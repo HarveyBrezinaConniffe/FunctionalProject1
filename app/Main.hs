d6e449cbbb105ae02d58e4c320cf63eab71f6007
Harvey Brezina Conniffe <harvey@conniffe.com>
2023-11-22 20:57:28 +0000

    Button choice works.

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 
 module Main (main) where
 
+import Control.Monad.IO.Class (MonadIO(liftIO))
 import Lib
 import Shapes
 import Codec.Picture
@@ -11,7 +12,9 @@ import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import Web.Scotty
 import Data.IORef
-import System.Eval
+import qualified Data.ByteString.Lazy as B
+import Data.Text.Lazy (toStrict)
+import Data.Text.Encoding (decodeUtf8)
 
 --exampleDrawing =  [ ((scale 0.5 0.25)<+>(translate 0.25 0.5)<+>(shear 0.25 0.1)<+>(rotate 0.5), circle) ]
 --circleDrawing = [(ident, circle)]
@@ -35,18 +38,23 @@ circleMask = [((scale 1 0.25), circle)]
 
 --main = render "output.png" defaultWindow exampleDrawing (MaskDrawing circleMask)
 
-main = scotty 8080 $ do
-  imgProg <- newIORef "[((translate 0.5 0), rectangle 0.5 0.5, gradient), ((scale 0.25 0.5), circle, gradient)]"
-  maskProg <- newIORef "[((scale 1 0.25), circle)]"
-  get "/image" $ do
-    imgstr <- readIORef imgProg
-    maskstr <- readIORef maskProg
-    imgCode <- eval imgstr
-    raw (render defaultWindow imgCode (MaskDrawing circleMask))
-  get "/" $
-    html $ renderHtml $
-      H.html $
-        H.body $ do
-          H.h1 "Welcome"
-          H.p "Hi"
-          H.img H.! A.src "/image"
+renderImg :: Int -> B.ByteString
+renderImg 0 = render defaultWindow exampleDrawing (MaskDrawing circleMask)
+renderImg 1 = render defaultWindow exampleDrawing (EmptyMask)
+
+main = do
+  progNum <- newIORef 1
+  scotty 8080 $ do
+    get "/:imgNum" $ do
+      imgNum <- param "imgNum"
+      let imgURL = "/image/"++imgNum
+      html $ renderHtml $
+        H.html $
+          H.body $ do
+            H.h1 "Welcome"
+            H.img H.! A.src (H.stringValue imgURL)
+            H.form H.! A.action "/0" $ do
+              H.input H.! A.type_ "submit" H.! A.value "D0"
+    get "/image/:imgNum" $ do
+      imgNum <- param "imgNum"
+      raw $ renderImg imgNum
